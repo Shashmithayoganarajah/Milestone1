f6ea094768a6174c3c25a15bb0d51517d5eb407c
Shashmithayoganarajah <139777522+Shashmithayoganarajah@users.noreply.github.com>
2025-05-05 12:15:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Trello trello = new Trello();
 
-        trello.postTweet(1, 101);
-        trello.postTweet(2, 102);
+        trello.postTweet(1,"hello", 101);
+        trello.postTweet(2, "Genspark 2025",102);
+        trello.postTweet(1,"Hi", 103);
+        trello.postTweet(3, "Welcome", 104);
 
         trello.follow(1, 2);
 
